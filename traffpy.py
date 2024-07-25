e3e601ec78c41a94e247b7d60bded8ceceaf4837
achyaa <22611130@students.uii.ac.id>
2024-07-25 17:04:18 +0700

    Update traffpy.py

--- traffpy.py
+++ traffpy.py
@@ -1,9 +1,14 @@
 import pickle
 import streamlit as st
 import numpy as np
+import os
+
+# Mendapatkan path absolut ke file model
+current_directory = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_directory, 'model.sav')
 
 # Membaca model
-traffic_model = pickle.load(open('model.sav', 'rb'))
+traffic_model = pickle.load(open(model_path, 'rb'))
 
 # Judul web
 st.title('Prediksi Traffic')
